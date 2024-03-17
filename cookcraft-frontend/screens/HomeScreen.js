50bee66eb4dc0260befff3e0b366162e6ce0e14c
Sajeel-Haider <sajeelshp1@gmail.com>
2024-03-17 15:56:52 +0500

    * added a tester project

--- cookcraft-frontend/screens/HomeScreen.js
+++ cookcraft-frontend/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 const HomeScreen = () => {
   return (
     <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
-      <Text> Home Screen</Text>
+      <Text> Screen</Text>
     </View>
   );
 };
