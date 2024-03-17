27fbffcbeb3517664b6d1210ad6c88d2530b396a
Ahmed949916 <ahmed40152@gmail.com>
2024-03-17 15:57:46 +0500

    Update HomeScreen.js

--- cookcraft-frontend/screens/HomeScreen.js
+++ cookcraft-frontend/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+    <View className="flex-1 justify-center items-center space-y-10 bg-orange-400">
       <Text> Home Screen</Text>
     </View>
   );
