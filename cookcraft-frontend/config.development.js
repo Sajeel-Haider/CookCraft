68df483fad6cbee745444aa407d62bd1dec60240
Sajeel-Haider <sajeelshp1@gmail.com>
2024-03-24 17:13:11 +0500

    testing

--- cookcraft-frontend/config.development.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  API_URL: "http://192.168.10.3:8080",
-  // other development-specific variables
-};
-
-export default config;
--- cookcraft-frontend/config.production.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  API_URL: "https://yourproductionapi.com",
-  // other production-specific variables
-};
-
-export default config;
